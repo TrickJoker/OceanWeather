c1dbe60ba21b154fced03dcb943f553c6cbd4a90
aster <1031539058@qq.com>
2022-11-13 15:09:04 +0800

    dev 分支测试提交2

--- app/src/main/java/com/aster/oceanweather/MainActivity.java
+++ app/src/main/java/com/aster/oceanweather/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //dev 分支测试提交
+        //dev 分支测试提交2
     }
 }
